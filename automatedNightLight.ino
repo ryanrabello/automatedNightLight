c245b85d281cfdec906906f9c79fbe567a8dc43b
asteroidice <ryan.s.rabello@gmail.com>
2016-07-13 15:56:54 -0700

    should work now, doesn't

--- automatedNightLight.ino
+++ automatedNightLight.ino
@@ -1,53 +1,69 @@
 //Constants
 const int led = 13;
-const int ledSwitch = 
+const int ledSwitch = 5;    //photo sensor reading for toggling the led on/off state
+const int ledBuffer = 7;    //photo sensor sensitivity when light is on. (this will hopefully reduce repeated on/off toggle.)
+const int ledOn = 255;
+const int ledOff = 0;
+const int timeToSleep = 10 * 60; //secconds
+const int photocellPin = 0;
+
+
 //GLobalVars
+int currentLedSwitch = ledSwitch;
+int sleepTime = 0;
+int ledSetting = 0;
+char mode = 'a';
 
 void setup() {
   //Initialize those output ports.
-  pinMode(led,OUTPUT);
-  analogWrite(led,0);
+  //pinMode(led,OUTPUT);
+  analogWrite(led,ledOff);
+  for(int i = 0; i<=255; i++){
+    analogWrite(led,i);
+    delay(100);
+  }
 }
 
 
 //Transistion from current to inputed values
-void fadeTo(int red, int green, int blue,int transitionTime) {
-  //Want 20 fps
-  float spf = 1.0/((float)transitionTime*9.0);
-  //int time = transitionTime / (transitionTime*20); //Get time percycle in microseconds.
-  float change[] = {
-    red - ledStates[0],green - ledStates[1],blue - ledStates[2]  }; //Difference in all the values.
-  //Run transition
-  for(float i=0; i <= 1.00;) {
-    i += spf;
-    //Set all Led to the right value
-    for(int j=0; j < 3; j++){
-      analogWrite(led[j], i*change[j] + (float)ledStates[j]);
+void fadeTo(int state,int transitionTime) {
+  int delay = abs(transitionTime/(state - ledSetting));
+  int steps = state - ledSetting;
+  if(steps < 0){
+    for(steps; steps <=0; steps++){
+      ledSetting--;
+      analogWrite(led, ledSetting);
     }
   }
-  ledStates[0] = red;
-  ledStates[1] = green;
-  ledStates[2] = blue;
+
 }
 
 
 void lightOn() {
-
+  if(ledSetting != ledOn){
+    fadeTo(ledOn, 1000);
+    ledSetting = ledOn;
+    sleepTime = 0;
+  }else {
+    sleepTime++;
+    if(sleepTime >= timeToSleep){
+      fadeTo(ledOff, 5000);
+      mode = 's';
+    }
+  }
 }
 
 void loop() {
-  int a = analogRead(photocellPin);
-  if(currentLedSwitch < a){
+  if(currentLedSwitch < analogRead(photocellPin)){
     //RESET
-    fadeTo(ledOff,ledOff,ledOff,1000);
+    fadeTo(ledOff,1000);
     mode = 'a';
-    mainDelay = 1000;
     currentLedSwitch = ledSwitch;
-    time = 0;
+    sleepTime = 0;
   }
   switch(mode) {
     case 'a'://awake check if lights go off
-      if(currentLedSwitch >= a){
+      if(currentLedSwitch >= analogRead(photocellPin)){
         lightOn();
       }
       break;
@@ -55,9 +71,6 @@ void loop() {
       //sleep mode
       delay(10000);
       break;
-    default:
-      mode = 'n';
-      break;
   }
-  delay(mainDelay);
+  delay(1000);
 }
