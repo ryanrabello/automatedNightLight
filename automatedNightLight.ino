49ac32e89ffb0718cd64fe6c3f21988e94b5abbb
asteroidice <ryan.s.rabello@gmail.com>
2016-07-25 13:50:21 -0700

    removed speaker code beginning testing for bugs

--- automatedNightLight.ino
+++ automatedNightLight.ino
@@ -1,6 +1,5 @@
-#include "pitches.h"
 //Initialize led pins
-int led[] = {9,5,6};
+int led[] = {3,5,6};
 
 //Other config options see https://github.com/asteroidice/automatedNightLight/ for detailed info on what they do.
 const int photocellPin = 0;
@@ -10,7 +9,6 @@ const int ledOff = 255;     //Led full of pwm value.
 const int ledOn = 150;        //led full pwm brightness.
 const int ledSwitch = 5;    //photo sensor reading for toggling the led on/off state
 const int ledBuffer = 7;    //photo sensor sensitivity when light is on. (this will hopefully reduce repeated on/off toggle.)
-const int speakerPin = 3;
 
 //Don Touch it!!!
 //These values are global variables becuase they need to be referenced later.
@@ -23,15 +21,7 @@ int time = 0;
 int mainDelay = 1000;
 char mode = 'a';
 int prevAnalogRead;
-int notes[] = {
-NOTE_B2 ,NOTE_C3 ,NOTE_D3 ,NOTE_E3 ,NOTE_F3 ,NOTE_G3 ,NOTE_A3 ,NOTE_B3 ,NOTE_C4 ,NOTE_D4 ,NOTE_E4 ,NOTE_F4 ,NOTE_G4 ,NOTE_A4 ,NOTE_B4 ,NOTE_C5 ,NOTE_D5 ,NOTE_E5 ,NOTE_F5 ,NOTE_G5 ,NOTE_A5 ,NOTE_B5 ,NOTE_C6 ,NOTE_D6
-};
-const int noteSize = 23;
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  2,3,4, 8, 16 , 32
-};
-const int durationSize = 4;
+int timeDelays[] = {2,3,4, 8, 16 , 32};
 
 void setup() {
   //Initialize those output ports.
@@ -41,6 +31,13 @@ void setup() {
   }
   //Get random brightness and use it as the seed.
   randomSeed(analogRead(0));
+  writeAll(ledOff);
+  delay(1000);
+  writeAll(ledOn);
+  delay(1000);
+  writeAll(ledOff);
+  delay(1000);
+
 }
 
 //set all the led's to the inputed value
@@ -76,27 +73,6 @@ void fadeTo(int red, int green, int blue,int transitionTime) {
   ledStates[2] = blue;
 }
 
-//Transition with sine curve
-void sineFadeTo(int red, int green, int blue,int transitionTime) {
-  //Want 20 fps
-  float spf = 3.14159265/((float)transitionTime*9.0);
-  //int time = transitionTime / (transitionTime*20); //Get time percycle in microseconds.
-  float change[] = {
-    red - ledStates[0],green - ledStates[1],blue - ledStates[2]  }; //Difference in all the values.
-  //Run transition
-  float sineI;
-  for(float i=0; i <= 1.00;) {
-    i += spf;
-    sineI=(sin(PI*i-PI*.5)+1)/2;
-    //Set all Led to the right value
-    for(int j=0; j < 3; j++){
-      analogWrite(led[j], sineI*change[j] + (float)ledStates[j]);
-    }
-  }
-  ledStates[0] = red;
-  ledStates[1] = green;
-  ledStates[2] = blue;
-}
 void lightOn() {
   //Turn light on for X minutes and then fade it to dark then turn off light and goto sleep mode (all the while checking if the lights came back on)
   if(ledStates[0] != ledOff || ledStates[1] != ledOff || ledStates[2] != ledOff) { //if light is on
@@ -114,9 +90,11 @@ void lightOn() {
   }
 
 }
+
 void changeLed(){
   changeLed(false);
 }
+
 void changeLed(boolean slow) {
   int ranLed[3];
   if((int)(random(0,4) + .5) == 0){
@@ -150,12 +128,10 @@ void nap() {
 }
 
 void randomPattern() {
-  int count = 5;
+  int randWait = 1000/timeDelays[(int)(random(0,4)+.5)];
+  int count = 7;
   int ranLed[3];
   while(count > 0) {
-    int randWait = 1000/noteDurations[(int)(random(0,durationSize)+.5)];
-    int randNote = (int)(random(0,noteSize) + .5);
-    tone(speakerPin, notes[randNote]);
     //Configure led array
     if((int)(random(0,4) + .5) == 0){
       //25% of time.
@@ -172,12 +148,10 @@ void randomPattern() {
           break;
       }
     }
-    fadeTo(ranLed[0],ranLed[1],ranLed[2],75);
-    //writeAll(ranLed[0],ranLed[1],ranLed[2]);
+    fadeTo(ranLed[0],ranLed[1],ranLed[2],100);
 
     delay(randWait*.75);
-    noTone(speakerPin);
-    //run atleast twice then there exists a probability of exiting.
+    //run atleast 4 times then there exists a probability of exiting.
     if(count < (int)(random(0,5)+.5))
       break;
     count--;
